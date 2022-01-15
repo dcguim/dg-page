2270adc172c42b8767368a4a4b296d31dfa3d8e9
dcguim <dcguim@gmail.com>
2022-01-15 23:30:19 +0100

    Add software engineering topics

--- build-site.el
+++ build-site.el
@@ -1,3 +1,26 @@
+;; set the package installation repository in the dgpage repo folder
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+;; melpa
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+;; init package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install dependencies
+(package-install 'htmlize)
+
+
+(require 'ob-shell)
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((latex . t)
+			     (java . t)))
+
 ;; configurations for html export for dgpage
 (require 'ox-publish)
 (setq org-publish-project-alist
@@ -10,7 +33,7 @@
     :publishing-function org-html-publish-to-html
     :headline-levels 4
     :auto-preamble t
-    :with-autor nil
+    :with-author nil
     :with-toc nil
     :section-number nil
     :time-stamp-file t)
@@ -22,8 +45,10 @@
     :publishing-function org-publish-attachment)
    ("dgpage" :components ("org-notes" "org-static"))
    ))
-(setq org-html-validation-link nil)
-(setq org-html-self-link-headlines nil)
+(setq org-html-validation-link nil
+      org-html-self-link-headlines nil
+      org-html-htmlize-output-type 'css)
+
 (org-publish-all t)
 
 
