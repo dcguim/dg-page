e4839b3a1e7c1a409c98288beff7e8341e2a157d
dcguim <dcguim@gmail.com>
2022-02-01 17:20:03 +0100

    First version of the knowledge repo

--- build-site.el
+++ build-site.el
@@ -11,15 +11,38 @@
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
-
 ;; install dependencies
 (package-install 'htmlize)
+(package-install 'auctex)
+(load "auctex.el" nil t t)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex) 
 
+;; set the path for the latex macports executables
+(setq exec-path (append exec-path '("/opt/local/bin")))
+;; solve the error "Not enough arguments to format string"
+(with-eval-after-load 'org
+  (plist-put org-format-latex-options :foreground "Black")
+  (plist-put org-format-latex-options :background "Transparent"))
+
+(setq org-latex-create-formula-image-program 'imagemagick)
+(setq org-preview-latex-default-process 'imagemagick)
+(add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images)
 
 (require 'ob-shell)
 (org-babel-do-load-languages
  'org-babel-load-languages '((latex . t)
 			     (java . t)))
+
+;; fix problem with org-html-publish-to-html latex pictures
+(with-eval-after-load 'org
+(defun org--get-display-dpi ()
+  "Get the DPI of the display.
+The function assumes that the display has the same pixel width in
+the horizontal and vertical directions."
+  (if (display-graphic-p)
+      (round (/ (display-pixel-height)
+        (/ (display-mm-height) 25.4)))
+    96.0)))
 
 ;; configurations for html export for dgpage
 (require 'ox-publish)
@@ -38,7 +61,7 @@
     :section-number nil
     :time-stamp-file t)
    ("org-static"
-    :base-directory "~/work/dgpage/org/static"
+    :base-directory "~/work/dgpage/org"
     :base-extension "css\\|js\\|png\\|ttf\\|txt\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
     :publishing-directory "~/work/dgpage/static"
     :recursive t
