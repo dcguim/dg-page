d98fd34831506eee73c7ee3ce6e937d7dd1735ec
dcguim <dcguim@gmail.com>
2023-12-21 10:16:34 +0100

    Add latex dependencies in a separated org file as opposed to ox-latex

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # dg-page
 Simple static webserver for a personal learning diary
+
+## Porting this to differnt environments
+The `publish.sh` file loads the init file `build-site.el`, note that if you
+simply call `emacs` command it will use the init file located at `~/.emacs`. This
+might impact the packages available to build this site.
+
+The path to the binaries must be configured, in order to properly port this, for
+instance an ubuntu machine using aptitude package manager might require in the
+`build-site.el` the following emacs `exec-path` configuration:
+
+```
+(setq exec-path (append exec-path '("/opt/local/bin")))
+```
+
+while a mac using homebrew the following:
+
+```
+(setq exec-path (append exec-path '("/opt/homebrew/")))
+```
+
+
+
--- build-site.el
+++ build-site.el
@@ -16,9 +16,11 @@
 (package-install 'auctex)
 (load "auctex.el" nil t t)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex) 
-
-;; set the path for the latex macports executables
-(setq exec-path (append exec-path '("/opt/local/bin")))
+(package-install 'ob-diagrams)
+(load "ob-diagrams.el")
+(require 'ob-diagrams)
+;; set the path for the latex brew executables
+(setq exec-path (append exec-path '("/opt/homebrew/")))
 ;; solve the error "Not enough arguments to format string"
 (with-eval-after-load 'org
   (plist-put org-format-latex-options :foreground "Black")
@@ -27,6 +29,16 @@
 (setq org-latex-create-formula-image-program 'imagemagick)
 (setq org-preview-latex-default-process 'imagemagick)
 (add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images)
+(org-babel-do-load-languages 'org-babel-load-languages
+                 '((latex . t)))
+
+(setq imagemagick '(imagemagick :programs ("latex" "convert")
+                                :description "pdf > png"
+                                :message "you need to install the programs: latex and imagemagick."
+                                :use-xcolor t
+                                :image-input-type "pdf"
+                                :image-output-type ...))
+
 
 (require 'ob-shell)
 (org-babel-do-load-languages
@@ -45,13 +57,30 @@ the horizontal and vertical directions."
         (/ (display-mm-height) 25.4)))
     96.0)))
 
+
+;;(require 'ox-latex)
+;;(with-eval-after-load 'ox-latex
+;;   (add-to-list 'org-latex-classes
+;;                '("article"
+;;                  "\\documentclass[a4paper,12pt]{article}
+;;                  \\usepackage{tikz}
+;;                  \\usepackage{pgfplots}
+;;                  \\usetikzlibrary{arrows}
+;;                  \\pgfplotsset{compat=1.11}"
+;;                  ("\\section{%s}" . "\\section{%s}")
+;;                  ("\\subsection{%s}" . "\\subsection{%s}")
+;;                  ("\\subsubsection{%s}" . "\\subsubsection{%s}")
+;;                  ("\\paragraph{%s}" . "\\paragraph{%s}")
+;;                  ("\\subparagraph{%s}" . "\\subparagraph{%s}"))))
+;;
 ;; configurations for html export for dgpage
 (require 'ox-publish)
 (setq org-publish-project-alist
  '(
    ("org-notes"
     :recursive t
-    :base-directory "~/work/dgpage/org/"
+    :base-directory "~/work/dgpage/org"
+    :exclude "\\(soft-eng/.*\\|data-eng/.*\\)"
     :base-extension "org"
     :publishing-directory "~/work/dgpage/static"
     :publishing-function org-html-publish-to-html
@@ -75,7 +104,8 @@ the horizontal and vertical directions."
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
       org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"/static/style.css\"/>
-                     <link rel=\"stylesheet\" type=\"text/css\" href=\"/static/code.css\"/>")
+                     <link rel=\"stylesheet\" type=\"text/css\" href=\"/static/code.css\"/><script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>
+<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>")
 
 (org-publish-all t)
 
